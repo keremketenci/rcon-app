4b4a0bddc3c60464be7de918878c0154e5b2f2e5
keremketencidev <keremketenci.dev@protonmail.com>
2025-10-25 22:32:22 +0300

    add i18n (translation)

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
 import "./App.css";
 
+// translation
+import { useTranslation } from "react-i18next";
+
 // components
 import { ThemeModeToggle } from "@/components/shadcn/Theme/theme-mode-toggle";
 // components ui
 import { Button } from "@/components/shadcn/ui/button";
 
 function App() {
+  // translation
+  const { t, i18n } = useTranslation();
+
   return (
     <main>
-      <h1>Hello World!</h1>
+      <h1 className="text-2xl font-bold">{t("app.title")}</h1>
       <div className="flex min-h-svh flex-col items-center justify-center">
         <Button>Click me</Button>
         <ThemeModeToggle />
+        <div>
+          <button
+            className="border px-3 py-1 rounded"
+            onClick={() => i18n.changeLanguage("en")}
+          >
+            🇺🇸 English
+          </button>
+          <button
+            className="ml-2 border px-3 py-1 rounded"
+            onClick={() => i18n.changeLanguage("tr")}
+          >
+            🇹🇷 Türkçe
+          </button>
+        </div>
       </div>
     </main>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "@/components/shadcn/Theme/theme-provider";
+import "@/lib/i18n";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
