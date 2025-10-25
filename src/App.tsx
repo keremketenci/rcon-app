24232ce9e3392acdf7fad3bd87a34cb854b0fff6
keremketencidev <keremketenci.dev@protonmail.com>
2025-10-25 22:16:33 +0300

    shadcn library added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import "./App.css";
 
-function App() {
+// components ui
+import { Button } from "@/components/shadcn/ui/button";
 
+function App() {
   return (
     <main>
       <h1>Hello World!</h1>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
+      </div>
     </main>
   );
 }
