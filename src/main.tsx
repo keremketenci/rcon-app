75e4756099bebaf4cff791bcd90128ae1fd4c383
keremketencidev <keremketenci.dev@protonmail.com>
2025-10-25 22:40:18 +0300

    clean up code

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 // react
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 // app
 import App from "./App";
+
 // theme
 import { ThemeProvider } from "@/components/shadcn/Theme/theme-provider";
+
 // translation
 import "@/lib/i18n";
 
