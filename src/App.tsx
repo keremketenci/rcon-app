385d89d5c473ed09904853c6942f57375029fbeb
keremketencidev <keremketenci.dev@protonmail.com>
2025-10-25 22:25:45 +0300

    add theme mode toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import "./App.css";
 
+// components
+import { ThemeModeToggle } from "@/components/shadcn/Theme/theme-mode-toggle";
 // components ui
 import { Button } from "@/components/shadcn/ui/button";
 
@@ -9,6 +11,7 @@ function App() {
       <h1>Hello World!</h1>
       <div className="flex min-h-svh flex-col items-center justify-center">
         <Button>Click me</Button>
+        <ThemeModeToggle />
       </div>
     </main>
   );
